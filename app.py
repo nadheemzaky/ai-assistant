9bc0b933616d838cb79be3b582bc41e54abcf26f
nadeem-server <nadheem@zakysoft.com>
2025-10-24 12:06:05 +0000

    letting flask know reverse proxy(nginx)

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ import models
 import context_handler
 import database
 from flask_cors import CORS
-
+from werkzeug.middleware.proxy_fix import ProxyFix
 # do not touch this
 load_dotenv()
 context_handler.init_db()
 app = Flask(__name__)
 CORS(app)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 app.secret_key = os.getenv('SECRET_KEY', secrets.token_hex(16))
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
